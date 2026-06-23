5843d33d7ef32ba8dce89aea460c3778789ff161
JAGAN546 <kjagannathsubudhi4@gmail.com>
2026-06-23 09:46:37 +0530

    Added first HackerRank Java solutions

---
+++ Loops1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Loops1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,n;
+        n = sc.nextInt();
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+        sc.close();
+    }
+}
