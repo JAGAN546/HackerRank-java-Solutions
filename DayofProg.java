e5ff09df4e9c995f5b07c6618d7ecc487eb3f16e
JAGAN546 <kjagannathsubudhi4@gmail.com>
2026-07-19 14:20:49 +0530

    Added Day of the Programmer of hackerrank

---
+++ DayofProg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DayofProg {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if (year == 1918) {
+            System.out.println("26.09.1918");
+        }
+        else if (year < 1918) {
+            if (year % 4 == 0)
+                System.out.println("12.09." + year);
+            else
+                System.out.println("13.09." + year);
+        }
+        else {
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                System.out.println("12.09." + year);
+            else
+                System.out.println("13.09." + year);
+        }
+    }
+}
